5e6d8806190340e5b4295351b0a3fc6e217019f4
camalzadeelvin <camalzadeelvin@STHQ012D-01>
2022-12-18 16:13:44 +0400

    Add project files.

---
+++ E-CommerceDapper/Domain/ViewModels/HomeViewModel.cs
@@ -0,0 +1,45 @@
+﻿using E_CommerceDapper.DataAccess.Entites;
+using E_CommerceDapper.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceDapper.Domain.ViewModels
+{
+    public class HomeViewModel:BaseViewModel
+    {
+		private ObservableCollection<Product> allProducts;
+
+		public ObservableCollection<Product> AllProducts
+        {
+			get { return allProducts; }
+			set { allProducts = value; OnPropertyChanged(); }
+		}
+
+		private Product selectedProduct;
+
+		public Product SelectedProduct
+		{
+			get { return selectedProduct; }
+			set { selectedProduct = value; OnPropertyChanged(); }
+		}
+		public RelayCommand	SelectedCommand { get; set; }
+		public HomeViewModel()
+		{
+			var productsFromDb = App.DB.ProductRepository.GetAll();
+			AllProducts = new ObservableCollection<Product>(productsFromDb);
+
+			SelectedProduct = new Product();
+
+
+			SelectedCommand = new RelayCommand((o) =>
+			{
+
+			});
+		}
+
+	}
+}
